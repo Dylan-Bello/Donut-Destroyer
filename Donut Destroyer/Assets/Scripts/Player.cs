ffd5e11a42369513419daf87c1465a054c0123db
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2021-02-20 13:02:00 +0800

    Fixed Joysticks

---
+++ Donut Destroyer/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Joystick joystickL;
+    public Joystick joystickR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveVec = new Vector2 (joystickL.Horizontal, joystickL.Vertical);
+
+
+        Vector3 lookVec = new Vector3 (joystickR.Horizontal, joystickR.Vertical, 4000);
+
+        transform.rotation = Quaternion.LookRotation(lookVec, Vector3.back);
+        transform.Translate(moveVec * Time.deltaTime * speed, Space.World);
+    }
+}
